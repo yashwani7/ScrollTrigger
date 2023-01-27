9d7539dd5936ff9f5f615fee0f05674951a9fa5a
yashwani7 <yashwaniverma2017@gmail.com>
2023-01-27 10:08:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+gsap.from("#txt",{
+    scrollTrigger:{
+        scrub:true
+    },
+    x:350,
+})
+
+gsap.from("#m1",{
+    scrollTrigger:{
+        scrub:true
+    },
+    y:100,
+})
+gsap.from("#m2",{
+    scrollTrigger:{
+        scrub:true
+    },
+    y:50,
+})
+gsap.from("#m3",{
+    scrollTrigger:{
+        scrub:true
+    },
+    y:200,
+})
+gsap.from("#m4",{
+    scrollTrigger:{
+        scrub:true
+    },
+    y:150,
+})
+gsap.from("#tr",{
+    scrollTrigger:{
+        scrub:true
+    },
+    y:150,
+})
+gsap.from("#crs",{
+    scrollTrigger:{
+        scrub:true
+    },
+    x:150,
+})
+gsap.from("#house",{
+    scrollTrigger:{
+        scrub:true
+    },
+    x:0,
+})
+gsap.from(".sec h2",{
+    scrollTrigger:{
+        scrub:true
+    },
+    x:-1000,
+})
+gsap.from(".sec p",{
+    scrollTrigger:{
+        scrub:true
+    },
+    x:1000,
+})
